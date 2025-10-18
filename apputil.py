137632351022639a42bb99f81cfcccf51ca94509
Serena Hawkins <sjhawkins@ap.org>
2025-10-17 20:09:57 -0400

    first stab at exercise 1

--- apputil.py
+++ apputil.py
@@ -9,9 +9,28 @@ class MarkovText(object):
 
     def get_term_dict(self):
 
-        # your code here ...
+        #First split the one string into a list of words 
+        split_words = self.corpus.split(" ")
+        print(split_words[0:5])
+        #get the unique words for the keys 
+        #I was origonally transforming this into a set and then a list to 
+        #get the unique words, but it didn't match the output given
+        unique_words = split_words
+        print(unique_words)
+        #Create empty dictionary
+        return_dict = defaultdict(list)
+        #Get a list of all the words in the corpus
+        for word in unique_words: 
+            indices =  [i for i, x in enumerate(split_words) if x == word]
 
-        self.term_dict = {}
+            for index in indices: 
+                try: 
+                    return_dict[word].append(split_words[index + 1])
+                except: 
+                    print(f"{word}, is the last word in the corpus. No further words")    
+
+
+        self.term_dict = {return_dict}
 
         return None
 
